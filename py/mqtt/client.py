374de37924485704cb5051dec9c753e5ef1027dc
tongjinlv <179990830@qq.com>
2016-07-04 09:37:31 +0000

    null

--- py/mqtt/client.py
+++ py/mqtt/client.py
@@ -5,10 +5,17 @@ import os
 import time 
 ss=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ss.connect(('112.74.98.39',1500))
-#ss.connect(('192.168.1.11',8080))
 ss.sendall('H718L9X19767N1G2')
 data=ss.recv(1024)
-print "server dafu %s"%data
-os.system('sleep 1')
-ss.sendall('{"sensorDatas":[{"value":13.43},{"value":44}]}\n')
-ss.close()
+print "revice %s"%data
+for i in range(1, 50):
+	os.system('sleep 1')
+	sdata='{"sensorDatas":[{"value":'+str(i)+'},{"value":44},{"value":4.20},{"value":23}]}\n'
+	ss.sendall(sdata)
+	print sdata
+	revice=ss.recv(1024)
+        print "revice %s"%revice
+
+else:
+    	print('发送完毕')
+	ss.close()
