bd5b25eb5a5c337520175fdc779db42d01e76fa4
tongjinlv <179990830@qq.com>
2017-12-11 17:38:52 -0800

    add opencv

---
+++ Projects/cmake/build/CMakeFiles/lesson3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson3.dir/main.o"
+  "lesson3.pdb"
+  "lesson3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lesson3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
