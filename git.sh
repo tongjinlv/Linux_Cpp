9828370bba9153395d3771316bf8c335653cb4f8
tongjinlv <179990830@qq.com>
2019-03-14 06:58:30 +0000

    add what?

--- git.sh
+++ git.sh
@@ -2,6 +2,6 @@ git config --global credential.helper store
 git config --global user.email "179990830@qq.com"
 git config --global user.name "tongjinlv"
 git add --all
-git commit -m "add opencv"
+git commit -m "add what?"
 git push	
 
